78461071068394dc8b2c09e7ff9e0f001acead87
unknown <milad.rezaeshirazi@gmail.com>
2024-02-23 20:22:49 +0330

    deployment ready

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,12 +4,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
 	return (
 		<Html lang="en">
-			<title>"Milad Shirazi</title>
+			<title>{`Milad Shirazi`}</title>
 
 			<Head>
 				<meta
 					name="description"
-					content={`جهت تماس برای مشاوره با تیم مجرب و شناخته شده در شرکت هرم سازه کهن جهت پذیرش پیمانکاری کلیه امور عمرانی و ساخت و ساز ساختمان تجاری، آپارتمانی، ویلایی و… با ما تماس بگیرید:`}
+					content={`Milad shirazi AI portfolio and usecases`}
 				/>
 				<link
 					rel="shortcut icon"
